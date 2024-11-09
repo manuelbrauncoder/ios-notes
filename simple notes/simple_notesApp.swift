75b96bf8df8fac5e429f2c6d36fc46aaae0ed8a2
Manuel Braun <manuel.braun.coder@gmail.com>
2024-11-09 11:08:07 +0100

    Initial Commit

---
+++ simple notes/simple_notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_notesApp.swift
+//  simple notes
+//
+//  Created by Manuel Braun on 09.11.24.
+//
+
+import SwiftUI
+
+@main
+struct simple_notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
